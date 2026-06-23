0f4ef1f0ceea9f14ae29c2ab6aceee65dc00a44c
Aman Kumar Maurya <amankrmj@gmail.com>
2026-06-23 20:46:40 +0530

    Add: application configuration and custom exceptions for template handling

---
+++ src/main/java/com/shah_s/bakery_notification_service/exception/DuplicateTemplateException.java
@@ -0,0 +1,7 @@
+package com.shah_s.bakery_notification_service.exception;
+
+public class DuplicateTemplateException extends RuntimeException {
+    public DuplicateTemplateException(String message) {
+        super(message);
+    }
+}
