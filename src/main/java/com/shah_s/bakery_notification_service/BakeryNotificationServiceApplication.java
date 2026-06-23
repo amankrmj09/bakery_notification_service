1e95cada0f32e4576c09d690cfda382062096de1
Aman Kumar Maurya <amankrmj@gmail.com>
2026-06-23 21:06:18 +0530

    Add: import MethodSecurityConfig for enhanced security configuration

--- src/main/java/com/shah_s/bakery_notification_service/BakeryNotificationServiceApplication.java
+++ src/main/java/com/shah_s/bakery_notification_service/BakeryNotificationServiceApplication.java
@@ -2,6 +2,8 @@ package com.shah_s.bakery_notification_service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Import;
+import org.devofblue.common.security.MethodSecurityConfig;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
@@ -9,6 +11,7 @@ import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@Import(MethodSecurityConfig.class)
 @EnableDiscoveryClient
 @EnableFeignClients
 @EnableCaching
@@ -21,3 +24,4 @@ public class BakeryNotificationServiceApplication {
     }
 
 }
+
