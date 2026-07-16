181ad08a1c7a508d0744f42a60a8d7f0e335a070
Aman Kumar Maurya <amankrmj@gmail.com>
2026-07-16 17:30:55 +0530

    fix: Migrate BrevoEmailClient from WebClient to RestClient

--- src/main/java/com/blubugtech/bakery_notification_service/config/BrevoClientConfig.java
+++ src/main/java/com/blubugtech/bakery_notification_service/config/BrevoClientConfig.java
@@ -3,17 +3,17 @@ package com.blubugtech.bakery_notification_service.config;
 import com.blubugtech.bakery_notification_service.client.BrevoEmailClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.reactive.function.client.WebClient;
-import org.springframework.web.reactive.function.client.support.WebClientAdapter;
+import org.springframework.web.client.RestClient;
+import org.springframework.web.client.support.RestClientAdapter;
 import org.springframework.web.service.invoker.HttpServiceProxyFactory;
 
 @Configuration
 public class BrevoClientConfig {
 
     @Bean
-    public BrevoEmailClient brevoEmailClient(WebClient.Builder webClientBuilder) {
-        WebClient webClient = webClientBuilder.build();
-        HttpServiceProxyFactory factory = HttpServiceProxyFactory.builderFor(WebClientAdapter.create(webClient)).build();
+    public BrevoEmailClient brevoEmailClient(RestClient.Builder restClientBuilder) {
+        RestClient restClient = restClientBuilder.build();
+        HttpServiceProxyFactory factory = HttpServiceProxyFactory.builderFor(RestClientAdapter.create(restClient)).build();
         return factory.createClient(BrevoEmailClient.class);
     }
 }
