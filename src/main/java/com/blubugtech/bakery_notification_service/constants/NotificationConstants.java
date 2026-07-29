2434d7ae4c2d53d33ef1ed6963d428745d03e415
Aman Kumar Maurya <amankrmj@gmail.com>
2026-07-29 21:08:49 +0530

    Refactor: Centralize domain constants and update core dependencies

--- src/main/java/com/blubugtech/bakery_notification_service/constants/NotificationConstants.java
+++ src/main/java/com/blubugtech/bakery_notification_service/constants/NotificationConstants.java
@@ -1,7 +1,7 @@
 package com.blubugtech.bakery_notification_service.constants;
 
-public class KafkaGroups {
+public final class NotificationConstants {
+    private NotificationConstants() {}
+
     public static final String NOTIFICATION_GROUP = "notification-group";
-    
-    private KafkaGroups() {}
 }
