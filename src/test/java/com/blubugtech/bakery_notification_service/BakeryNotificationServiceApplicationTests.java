e3b9c7e8c29185f7a4a85af3a80631cab90e17d0
Aman Kumar Maurya <amankrmj@gmail.com>
2026-08-01 10:24:33 +0530

    chore(config): update configs and tests, remove unused docker file

--- src/test/java/com/blubugtech/bakery_notification_service/BakeryNotificationServiceApplicationTests.java
+++ src/test/java/com/blubugtech/bakery_notification_service/BakeryNotificationServiceApplicationTests.java
@@ -2,7 +2,9 @@ package com.blubugtech.bakery_notification_service;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 @SpringBootTest
+@ActiveProfiles("test")
 class BakeryNotificationServiceApplicationTests {
 
     @Test
