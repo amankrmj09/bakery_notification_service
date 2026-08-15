08095de09be273b5c0feb9fe6b12eed86ce60e4f
Aman Kumar Maurya <amankrmj@gmail.com>
2026-08-15 17:17:27 +0530

    chore: cleanup obsolete constants, enums, and DTOs

--- src/main/java/com/blubugtech/bakery_notification_service/constants/NotificationConstants.java
+++
@@ -1,7 +0,0 @@
-package com.blubugtech.bakery_notification_service.constants;
-
-public final class NotificationConstants {
-    private NotificationConstants() {}
-
-    public static final String NOTIFICATION_GROUP = "notification-group";
-}
--- src/main/java/com/blubugtech/bakery_notification_service/enums/NotificationType.java
+++
@@ -1,5 +0,0 @@
-package com.blubugtech.bakery_notification_service.enums;
-
-public enum NotificationType {
-    ORDER_CONFIRMATION, PASSWORD_RESET, WELCOME, GENERAL
-}
